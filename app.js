d28729cfb2aad1705c9952aeb97c179a78e26669
mayurk-amdocs <mayurkas@amdocs.com>
2023-09-04 12:31:06 +0000

    feat: add worker

---
+++ app.js
@@ -0,0 +1,42 @@
+const amqp = require("amqplib/callback_api");
+
+amqp.connect("amqp://localhost", function (error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+
+  connection.createChannel((err, channel) => {
+    if (err) {
+      throw err;
+    }
+
+    console.log('Channel established ... \n');
+
+    const queue = 'ti.rpc.worker';
+    const exchange = "bpmn.rpc";
+    const routingKey = 'rpc.request'
+
+    channel.assertExchange(exchange, "topic", {
+      durable: true, autoDelete: true
+    });
+
+    channel.assertQueue(queue, {autoDelete: true, durable: true}, (error_1)=> {
+      if(error_1) {
+        throw error_1;
+      }
+
+      channel.bindQueue(queue, exchange, routingKey);
+    
+      channel.consume(queue, (msg)=> {
+        const replyTo = msg.properties.replyTo;
+        console.log("replying to -> ", replyTo)
+        
+        const content = Buffer.from(JSON.stringify(msg.content));
+        channel.sendToQueue(replyTo, content, {
+          correlationId: msg.properties.correlationId,
+          persistent: true
+        });
+      }, {noAck: true})
+    });
+  });
+});
